8a8e1a9af9bc2c31853ab51d7a42b3b45e2b058c
Graciana Amaranto <86271555+graciana-amaranto@users.noreply.github.com>
2023-01-23 01:50:31 -0300

    WindowsFactura

---
+++ WindowsFactura/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFactura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProd1 = new System.Windows.Forms.TextBox();
+            this.txtProd2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtProd1
+            // 
+            this.txtProd1.Location = new System.Drawing.Point(263, 128);
+            this.txtProd1.Multiline = true;
+            this.txtProd1.Name = "txtProd1";
+            this.txtProd1.ReadOnly = true;
+            this.txtProd1.Size = new System.Drawing.Size(280, 65);
+            this.txtProd1.TabIndex = 0;
+            // 
+            // txtProd2
+            // 
+            this.txtProd2.Location = new System.Drawing.Point(263, 280);
+            this.txtProd2.Multiline = true;
+            this.txtProd2.Name = "txtProd2";
+            this.txtProd2.ReadOnly = true;
+            this.txtProd2.Size = new System.Drawing.Size(280, 55);
+            this.txtProd2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtProd2);
+            this.Controls.Add(this.txtProd1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProd1;
+        private System.Windows.Forms.TextBox txtProd2;
+    }
+}
---
+++ WindowsFactura/Form1.cs
@@ -0,0 +1,55 @@
+﻿using LibFactura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFactura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Producto producto1 = new Producto() { NombreProducto = "Cuadro de Mona Lisa" };
+            Producto producto2 = new Producto() { NombreProducto = "Escultura El Pensador" };
+            List<Producto> productos = new List<Producto>() { producto1, producto2 };
+
+            Categoria pintura = new Categoria() { CategoriaProducto = "Pintura" };
+            producto1.Categoria = pintura;
+            Categoria escultura = new Categoria() { CategoriaProducto = "Escultura" };
+            producto2.Categoria = escultura;
+
+            Cliente cliente1 = new Cliente(1, "San Jose 133", "Juan", "Perez", 34526302);
+            Usuario usuario = new Usuario("pepe24", "68754sdr", "Jose", "Ramirez", 59452325);
+            Empleado empleado = new Empleado(5, "Gerente", "Luz", "Gonzales", 14785693);
+            Factura factura = new Factura("Accenture");
+
+            empleado.Facturas = new List<Factura> { factura };
+            cliente1.Facturas = new List<Factura> { factura };
+
+            DetalleFactura detalleFactura = new DetalleFactura() { Descripcion = "Compra en efectivo"};
+            factura.DetalleFacturas = new List<DetalleFactura> { detalleFactura };
+
+            producto1.DetalleFactura = detalleFactura;
+            producto2.DetalleFactura = detalleFactura;
+
+            txtProd1.Text = $"Producto 1: {producto1.NombreProducto}, Detalle de factura: {producto1.DetalleFactura},  Categoria: {producto1.Categoria} ";
+
+            txtProd2.Text = $"Producto 2: {producto2.NombreProducto}, Detalle de factura: {producto2.DetalleFactura},  Categoria: {producto2.Categoria} ";
+
+        }
+
+       
+    }
+}
